19f62e66c3ecf789a30319abe955c8ff65791753
Aakriti Gupta <draakritig@gmail.com>
2020-10-26 19:41:39 -0400

    Finally Done

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost:27017/books229"
+  //"URI": "mongodb://localhost:27017/books229"
+  "URI": "mongodb+srv://aakritig:RaJSX25dxInuYdxA@mongodbserver.8uile.mongodb.net/books229?retryWrites=true&w=majority"
 };
